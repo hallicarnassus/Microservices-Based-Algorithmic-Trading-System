73dee1e3829e1c354d48523f5f422b33cb9b4c08
Doc <hallicarnassus@users.noreply.github.com>
2021-01-21 09:09:47 +1100

    Update oanda_cred.py
    Update py

--- Storage/q_pack/q_credentials/oanda_cred.py
+++ Storage/q_pack/q_credentials/oanda_cred.py
@@ -1,2 +1,2 @@
-acc_id_practice="luke.nyhof@gmail.com"
-token_practice="0b02cf1ce4e6f2e8ae9c5c270b8c8b90-90655ddc9d88073e69580c9478507861"
+acc_id_practice="name@email.com"
+token_practice="OANDA_API_KEY_GOES_HERE"
